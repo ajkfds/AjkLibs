611d4809db2f44085e6b5a5326fc4f0512d50868
ajkfds <ajkfds2018@gmail.com>
2026-04-19 10:51:24 +0900

    initial commit

---
+++ AjkLibs/FileBasedPipe/CommunicationEnvelope.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AjkLib.FileBasedPipe
+{
+    public class CommunicationEnvelope
+    {
+        // 送信側が「現在保持している未送信リスト」を格納
+        public List<MessageItem> Messages { get; set; } = new();
+    }
+}
